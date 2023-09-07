de5a364c2f5b6a826baba9b065bc964d30fe73aa
Gwahir <Gwahir@gmail.com>
2023-09-07 14:59:35 -0400

    Update git-sync.sh to fetch first

--- git-sync.sh
+++ git-sync.sh
@@ -48,4 +48,5 @@ if [[ -n "$DESTINATION_SSH_PRIVATE_KEY" ]]; then
   git config --local core.sshCommand "/usr/bin/ssh -i ~/.ssh/dst_rsa"
 fi
 
+git fetch
 git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}"
